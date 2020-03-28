820031e6588888c131facff1bd9d41411192ab3b
willian14 <willian.bandeira.dossantos@hotmail.com>
2020-03-28 19:22:37 -0400

    aula 05 projeto web

---
+++ src/main/java/ClienteServlet.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = {"/cliente.do", "/clienteController", "/clienteServlet"})	
+public class ClienteServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método Get!!!!");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método Post!!!");
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método Delete!");
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método Put!");
+	}
+
+}
